72a57614756177fd4739e7318e8bc11697ca3fe4
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-07-17 21:49:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+let render = (leads) => {
+    let listItems = ""
+    for(let i = 0; i < leads.length; i++) {
+        listItems += 
+        `<li>
+        <a target="_blank" href ="${leads[i]}">${leads[i]}</a></li>`
+    }
+    ulEl.innerHTML = listItems
+}
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
 })
 
-let renderLeads = () => {
-    let listItems = ""
-    for(let i = 0; i < myLeads.length; i++) {
-        listItems += 
-        `<li>
-        <a target="_blank" href ="${myLeads[i]}">${myLeads[i]}</a></li>`
-    }
-    ulEl.innerHTML = listItems
-}
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, currentWindow: true},(tabs) => {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
